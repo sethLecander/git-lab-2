6f2065b0580eac6c236ded399d4331a9047094a9
Seth Lecander <sl503819@ohio.edu>
2019-09-04 16:49:13 -0400

    commit again?

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Seth Lecander
+ *        Date: September 4th, 2019
+ * Description: yo
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
